475f1900add7dcd49c5a5203e9e199661b17a62a
hirayui1 <playerhasbeentoastedby@gmail.com>
2025-11-28 15:45:29 +0100

    lst functions extension

---
+++ lst_ops2.c
@@ -0,0 +1,15 @@
+#include "push_swap.h"
+
+t_stack *find_smallest(t_stack *a)
+{
+        t_stack *smallest;
+
+        smallest = a;
+        while (a->next)
+        {
+                if (a->next->x < smallest->x)
+                        smallest = a->next;
+                a = a->next;
+        }
+        return (smallest);
+}
