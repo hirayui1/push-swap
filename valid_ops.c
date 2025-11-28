f9c9c0651d56806fa42a44511350149f7f4611e5
hirayui1 <playerhasbeentoastedby@gmail.com>
2025-11-28 20:41:17 +0100

    norm fixes, more to be done

---
+++ valid_ops.c
@@ -0,0 +1,77 @@
+#include "push_swap.h"
+
+int	validate_input(char *argv)
+{
+	int	j;
+
+	j = 0;
+	while (argv[j])
+	{
+		if (argv[j] != '-' && !ft_isdigit(argv[j]))
+		{
+			write(2, "Error\n", 6);
+			return (0);
+		}
+		j++;
+	}
+	return (1);
+}
+
+int	is_dup(char **argv, int i)
+{
+	long	curr;
+	int		j;
+
+	while (argv[i])
+	{
+		curr = ft_atoi(argv[i]);
+		j = i + 1;
+		while (argv[j])
+		{
+			if (ft_atoi(argv[j]) == curr)
+			{
+				write(2, "Error\n", 6);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	is_minmax(char **argv, int i)
+{
+	long	curr;
+
+	while (argv[i])
+	{
+		curr = ft_atoi(argv[i]);
+		if (curr > INT_MAX || curr < INT_MIN)
+		{
+			write(2, "Error\n", 6);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	is_sorted(char **argv, int i)
+{
+	int	last;
+	int	curr;
+
+	if (is_minmax(argv, i) || is_dup(argv, i))
+		return (0);
+	last = ft_atoi(argv[i]);
+	while (argv[++i])
+	{
+		curr = ft_atoi(argv[i]);
+		if (curr < last)
+			return (1);
+		else
+			last = curr;
+	}
+	return (0);
+}
